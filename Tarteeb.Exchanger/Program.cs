c990a7543704e40e4e5b3c8981f209d4af30b028
Mirsaid04 <mirsaidd04@gmail.com>
2023-11-13 17:14:57 +0900

    Add project files.

---
+++ Tarteeb.Exchanger/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarteeb.Exchanger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
